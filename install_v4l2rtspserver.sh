ba006f2dedf24de0b7d90dce6bea9844ac7a7723
Marko Kivijarvi <marko.kivijarvi@pintplease.com>
2021-02-23 13:50:36 +0200

    Updates to rtsp again

--- install_v4l2rtspserver.sh
+++ install_v4l2rtspserver.sh
@@ -1,5 +1,5 @@
 curl https://www.linux-projects.org/listing/uv4l_repo/lpkey.asc | sudo apt-key add -
-sudo echo "\n\ndeb http://www.linux-projects.org/listing/uv4l_repo/raspbian/stretch stretch main\n" >> /etc/apt/sources.list
+echo "\n\ndeb http://www.linux-projects.org/listing/uv4l_repo/raspbian/stretch stretch main\n" | sudo tee -a /etc/apt/sources.list
 
 sudo apt update
 sudo apt install cmake liblog4cpp5-dev libv4l-dev git vim uv4l uv4l-server uv4l-raspicam uv4l-raspicam-extras
