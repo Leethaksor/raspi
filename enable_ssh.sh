ea451b03286650b5b761fa67c18d74811fe63727
Marko Kivijarvi <marko.kivijarvi@pintplease.com>
2021-03-12 15:36:38 +0200

    Add sudo

--- enable_ssh.sh
+++ enable_ssh.sh
@@ -1,5 +1,5 @@
-ssh-keygen -A &&
-update-rc.d ssh enable &&
-invoke-rc.d ssh start &&
+sudo ssh-keygen -A &&
+sudo update-rc.d ssh enable &&
+sudo invoke-rc.d ssh start &&
 echo "SSH enabled"
 
--- install_v4l2rtspserver.sh
+++ install_v4l2rtspserver.sh
@@ -1,16 +1,16 @@
 curl https://www.linux-projects.org/listing/uv4l_repo/lpkey.asc | sudo apt-key add -
-echo "\n\ndeb http://www.linux-projects.org/listing/uv4l_repo/raspbian/stretch stretch main\n" | sudo tee -a /etc/apt/sources.list
+echo "deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/stretch stretch main" | sudo tee -a /etc/apt/sources.list
 
 sudo apt update
-sudo apt install cmake liblog4cpp5-dev libv4l-dev git vim uv4l uv4l-server uv4l-raspicam uv4l-raspicam-extras
-sudo cp -f uv4l-raspicam.conf /etc/uv4l/
+sudo apt install cmake liblog4cpp5-dev libv4l-dev git vim uv4l uv4l-server uv4l-raspicam uv4l-raspicam-extras -y
+sudo cp -f ./uv4l-raspicam.conf /etc/uv4l/uv4l-raspicam.conf
 
-sudo cp -f ./uv4l_raspicam.service /etc/systemd/system/uv4l_raspicam.service/
+sudo cp -f ./uv4l_raspicam.service /etc/systemd/system/uv4l_raspicam.service/uv4l_raspicam.service
 
-sudo cp -f ./uv4l-time-overlay.cpp /usr/share/uv4l/raspicam/
+sudo cp -f ./uv4l-time-overlay.cpp /usr/share/uv4l/raspicam/uv4l-time-overlay.cpp
 cd /usr/share/uv4l/raspicam/
 sudo g++ -o uv4l-time-overlay uv4l-time-overlay.cpp -std=c++11
-sudo cp -f ./uv4l_raspicam_text.service /etc/systemd/system/
+sudo cp -f ./uv4l_raspicam_text.service /etc/systemd/system/uv4l_raspicam_text.service
 
 cd
 git clone https://github.com/mpromonet/v4l2rtspserver.git
